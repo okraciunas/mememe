d10a6a7e4921c615561b7850f2a0fb956d4d103b
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2019-01-16 22:48:10 -0200

    Implementando Toolbar. Abrindo o ImagePicker e carregando a imagem selecionada.

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by terced-leonardoo on 16/01/19.
+//  Copyright © 2019 OKraciunas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imagePickerView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+}
